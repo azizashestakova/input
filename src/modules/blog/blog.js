92826e351e99c0d4a1811aba23a90d6d4d16d067
Aziza Shestakova <aziza.shestakova@nutnet.ru>
2019-05-23 16:11:26 +0300

    Truncate a String

---
+++ src/modules/blog/blog.js
@@ -0,0 +1,14 @@
+import '@babel/polyfill';
+
+'use strict';
+
+export default class Blog {
+  constructor() {
+    const strings = [...document.querySelectorAll('.blog__post-description')];
+    strings.forEach(string => {
+      if (string.innerHTML.length > 100) {
+        string.innerHTML = `${string.innerHTML.substring(0, 100)}...`;
+      }
+    });
+  }
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,9 @@
 import Icon               from '../modules/icon/icon';
 import Combobox           from '../modules/combobox/combobox';
 import Header             from '../modules/header/header';
+import Blog               from '../modules/blog/blog';
 
 new Icon();
 new Combobox();
 new Header();
+new Blog();
